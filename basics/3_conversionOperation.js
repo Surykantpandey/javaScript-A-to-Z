ee75e38babf6ff0eb9d72b0237c182c3d8508604
surykant panday <90472804+Surykantpandey@users.noreply.github.com>
2023-12-25 10:22:04 +0000

    comparision of datatypes

--- basics/3_conversionOperation.js
+++ basics/3_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "surykant"
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let valueInNumber = Number(score)
-console.log(score)
-console.log(typeof valueInNumber); 
-console.log(valueInNumber);
+// console.log(score)
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -15,7 +15,7 @@ console.log(valueInNumber);
 let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,5 +24,12 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 22
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************** Operations *******************//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
